0a36d7b8863cc088c43cf55fedc27e3ae8dfe68e
Christian Alvarez <christiandavidalvarez@gmail.com>
2023-08-24 01:17:15 -0500

    se realizan aserciones de servicios de edicion

--- cypress/integration/api-pet-store/apiPetstore.spec.js
+++ cypress/integration/api-pet-store/apiPetstore.spec.js
@@ -1,19 +1,23 @@
-function getRandomArbitrary(min, max) {
-    const upper = max + 1                      // random() upper limit is excluded
-    return Math.floor(Math.random() * (max - min) + min)
-  }
+    function getRandomArbitrary(min, max) {
+        const upper = max + 1                      // random() upper limit is excluded
+        return Math.floor(Math.random() * (max - min) + min)
+    }
 
-  let id=getRandomArbitrary(105,905)
-context('peticiones Rest',()=>{
+    let id_generated=getRandomArbitrary(105,905)
+    let urlHostPetStore='https://petstore.swagger.io/v2/pet'
+    let nameChanged='Puppy'.concat(id_generated)
+    let statusChanged='sold'
+
+    describe('peticiones Rest de una Pet Store',()=>{
 
     it('Agregar una mascota a la tienda mediante un servicio POST',()=>{
-        cy.request('POST','https://petstore.swagger.io/v2/pet',{
-            id: 101,
+        cy.request('POST',urlHostPetStore,{
+            id: id_generated,
             category: {
-              id: id,
-              name: 'raza-1'
+              id: id_generated,
+              name: 'raza'.concat(id_generated)
             },
-            name: 'dinosaur',
+            name: 'dinosaur'.concat(id_generated),
             photoUrls: [
               'string'
             ],
@@ -28,6 +32,40 @@ context('peticiones Rest',()=>{
             cy.log(response.body)
             expect(response.status).to.eq(200)
           })
+    })
+
+    it('Consultar la mascota agregada a la tienda de mascotas',()=>{
+        cy.request(urlHostPetStore.concat("/",id_generated)).then((response)=>{
+            cy.log(response.body)
+            expect(response.status).to.eq(200)
+            expect(response.body).property('name').to.eq('dinosaur'.concat(id_generated))
+            expect(response.body).property('status').to.eq('available')
+        })
+    })
+
+    it('Modificar el nombre y el status de la mascota agregada',()=>{
+        cy.request({
+            url: urlHostPetStore.concat("/",id_generated),
+            method: 'POST',
+            headers:{
+               'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'name='+nameChanged+'&status='+statusChanged
+        }).then((response)=>{
+            cy.log(response)
+            expect(response.status).to.eq(200)
+        })
+    })
+
+    it('Consultar que la modificacion de mascota se haya realizado con los valores cambiados',()=>{
+        var bodyChangedPet=''
+        cy.request(urlHostPetStore.concat('/','findByStatus','?status=',statusChanged)).
+        then((response)=>{
+            cy.log(response.body)
+            bodyChangedPet=response.body.filter(function(x){ return x.id == id_generated; })
+            cy.log(bodyChangedPet)
+           expect(bodyChangedPet[0]).property('name').to.eq(nameChanged)
+        })
     })
 })
 
